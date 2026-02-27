af93e861022dcd543b741ef26c62e7681d5a3b2d
Nihal Naser Siddiqui <nihal070502@gmail.com>
2026-02-27 14:44:13 +0530

    req added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Nihal.S",
+    description="A package for US Visa prediction",
+    packages=find_packages(),
+)
