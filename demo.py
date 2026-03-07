b215e2ccb604ed3dfeada27ecf17149363f77e8c
Nihal Naser Siddiqui <nihal070502@gmail.com>
2026-03-08 00:53:17 +0530

    Data Ingestion Added

--- demo.py
+++ demo.py
@@ -1,9 +1,16 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
-logging.info("This is exception check")
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
+# logging.info("This is exception check")
 
-try:
-    a=1/0
-except Exception as e:
-    raise USvisaException(e,sys)
+# try:
+#     a=1/0
+# except Exception as e:
+#     raise USvisaException(e,sys)
+
+
+
+from us_visa.pipline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline() 
