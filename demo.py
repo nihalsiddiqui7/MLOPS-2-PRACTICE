66ba987f3c08d56f2959802879ca2060487efdfb
Nihal Naser Siddiqui <nihal070502@gmail.com>
2026-03-07 18:29:33 +0530

    added exception and logger

---
+++ demo.py
@@ -0,0 +1,9 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+logging.info("This is exception check")
+
+try:
+    a=1/0
+except Exception as e:
+    raise USvisaException(e,sys)
